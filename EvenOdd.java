fbb305417cd1ee82cac81b9135c75b0874620990
Muhammad Febriyan A <muh.febrian@gmail.com>
2023-06-24 00:08:37 +0700

    First Study Java

---
+++ EvenOdd.java
@@ -0,0 +1,17 @@
+public class EvenOdd {
+    public static void main(String[] args) {
+        int number = 100;
+
+        for (int i = 1; i <= number; i++) {
+            if(i % 3 == 0){
+                  System.out.println("Fizz");
+            }else if(i % 5 == 0){
+                System.out.println("Buzz");
+            }else if(i % 3 == 0 && i % 5 == 0){
+                System.out.println("FizzBuzz");
+            }else{
+                System.out.println(i);
+            }
+        }
+    }
+}
